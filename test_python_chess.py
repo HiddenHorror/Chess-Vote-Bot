2052d7407b3aab883e2ff9c6747858dd7c066b9b
HiddenHorror <schaf.lan@outlook.de>
2021-04-17 17:21:39 +0200

    Initial Commit

---
+++ test_python_chess.py
@@ -0,0 +1,14 @@
+import chess
+
+from collections import defaultdict
+
+L = [1,2,45,55,5,4,4,4,4,4,4,5456,56,6,7,67]
+Lal = ["abc","abc","abc","abc","de","de"]
+print(max(Lal,key=Lal.count))
+
+#d = defaultdict(int)
+
+# for i in L:
+#     d[i] += 1
+# result = max(d.iteritems(), key=lambda x: x[1])
+# print(result)
